8773a78c4ee69f420dce711cf7777ef9471090e4
KC Drez <kcdrez@gmail.com>
2026-05-19 06:45:36 -0600

    updated entrypoint port default

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
+python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
